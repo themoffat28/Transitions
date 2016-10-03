8a105d46a5ce018a4c23aad8c6becf417acd0b3d
themoffat28 <bthemoffat@gmail.com>
2016-10-03 07:57:17 -0600

    Linked added view to associated ViewController.swift

---
+++ Transitions/Controller/UpdatedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpdatedViewController.swift
+//  Transitions
+//
+//  Created by Moffat, Braeden on 10/3/16.
+//  Copyright © 2016 Moffat, Braeden. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+override func viewDidLoad()
+    {
+        //Load the parent version of a view.
+        super.viewDidLoad()
+        //Load this pages components.
+    }
+}
