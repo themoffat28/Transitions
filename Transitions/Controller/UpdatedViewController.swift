6be54e8ab4e6e90cef7f413b7ccf8739da6104ad
themoffat28 <bthemoffat@gmail.com>
2016-10-03 08:51:44 -0600

    Inserted Second image.

--- Transitions/Controller/UpdatedViewController.swift
+++ Transitions/Controller/UpdatedViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class UpdatedViewController : UIViewController
 {
+    @IBOutlet private weak var TopImage: UIImageView!
+    @IBOutlet private weak var BottomImage: UIImageView!
+    
+    
 override func viewDidLoad()
     {
         //Load the parent version of a view.
         super.viewDidLoad()
         //Load this pages components.
+        BottomImage.image = UIImage(named: "justin-bieber-young-boy")
     }
 }
